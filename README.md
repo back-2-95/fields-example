51dc0edb252aac59b36087f312be68ca12fddffb
Marko Korhonen <marko.korhonen@gmail.com>
2016-03-14 07:26:34 +0200

    Default example evolving

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fields-example
 Example repo of using Fields API
+
+## TODO
+
+Factory-classes which takes in EntityConfiguration and
+creates framework specific implementations. e.g. :
+
+Zend Framework 2
+
+- Zend Form
+- Zend InputFilter
+- Zend View
+- Zend Validate
--- public/index.php
+++ public/index.php
@@ -11,10 +11,80 @@ $api->setStorage($storage);
 
 $entity_configuration = $api->getEntityConfiguration('track');
 
-echo "Hello world2<pre>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Fields API example</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
 
-print_r($entity_configuration);
+<div class="jumbotron">
 
-foreach ($entity_configuration->getFields() as $fields) {
-    echo 1;
-}
+    <div class="container">
+
+        <h1><?= $entity_configuration->getName() ?></h1>
+
+        <p><?= $entity_configuration->getDescription() ?></p>
+
+    </div>
+
+</div>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-12">
+
+        <form name="<?= $entity_configuration->getName() ?>" action="GET">
+        <?php foreach ($entity_configuration->getFields() as $fname => $field): if (isset($field->form)): ?>
+
+            <div class="form-group">
+
+                <label><?= $field->name ?></label>
+
+                <?php if ($field->form->widget === 'text'): ?>
+
+                <input name="<?= $fname ?>" type="text" class="form-control" placeholder="Text input">
+
+                <?php elseif ($field->form->widget === 'editor'): ?>
+
+                <textarea name="<?= $fname ?>" class="form-control editor" rows="3"></textarea>
+
+                <?php elseif ($field->form->widget === 'image'): ?>
+
+                <input name="<?= $fname ?>" type="file">
+
+                <?php elseif ($field->form->widget === 'tags'): ?>
+
+                <input name="<?= $fname ?>" type="file">
+
+                <?php endif; ?>
+
+            </div>
+
+        <?php endif; endforeach; ?>
+
+        <button type="submit" class="btn btn-default">Submit</button>
+
+        </form>
+
+        </div>
+    </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/bootstrap.min.js"></script>
+<script src="//cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
+
+<script>
+
+CKEDITOR.replaceAll('editor', function(textarea, config) {
+    config.toolbar = 'Basic';
+});
+
+</script>
+
+</body>
+</html>
